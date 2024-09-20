4f61de59c7fd29a811a2be9d95a078bd23463a2a
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-09-20 10:24:14 -0700

    fix module call

--- deregister.tf
+++ deregister.tf
@@ -4,4 +4,5 @@ module "registration" {
   github_org_name                  = var.github_org_name
   github_token_secret              = data.aws_secretsmanager_secret.github_token.name
   registration_token_secret_prefix = local.registration_token_secret_prefix
+  tags                             = local.default_module_tags
 }
