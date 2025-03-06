574e425bcdb63f479463dcc3f4ce2b9aaa368183
xOtanix <maksym@tinyfish.io>
2025-03-06 21:42:32 +0200

    add autoscaling group name to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "runner_role_arn" {
   description = "An actions runner EC2 instance role ARN."
   value       = module.instance-profile.instance_role_arn
 }
+
+output "autoscaling_group_name" {
+  description = "Autoscaling group name."
+  value       = aws_autoscaling_group.actions-runner.name
+}
